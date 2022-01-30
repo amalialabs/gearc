341a3f3b2d210a26c638d56eb0934413db2792b5
Elena Weiss <weisse@cip.ifi.lmu.de>
2022-01-30 16:23:01 +0100

    pom auf java11, handler test of optionparser

--- secretlab1/src/main/java/Handler.java
+++ secretlab1/src/main/java/Handler.java
@@ -1,8 +1,25 @@
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+import java.io.File;
+import java.util.ArrayList;
+
+
 public class Handler {
 
     public static void main(String[] args) {
         //parse parameters#
         //print help for calling cmd
+        OptionParser optionParser = new OptionParser("");
+        OptionSet options = optionParser.parse("-genelist");
+
+        optionParser.accepts("genelist").withRequiredArg();
+
+        optionParser.accepts("expectedChange").withOptionalArg();
+        optionParser.accepts("FC").withOptionalArg();
+        optionParser.accepts("pval").withOptionalArg();
+
+
     }
 
 }
