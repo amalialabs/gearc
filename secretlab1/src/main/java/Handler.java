5fa47a9c14347137ffd3842904ec5fe4cd38b702
Elena Weiss <weisse@cip.ifi.lmu.de>
2022-01-30 18:08:41 +0100

    parser functions

--- secretlab1/src/main/java/Handler.java
+++ secretlab1/src/main/java/Handler.java
@@ -1,25 +1,33 @@
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
-
 import java.io.File;
-
+import java.io.IOException;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
 
 public class Handler {
 
+    public enum expected_change {LOW, AVERAGE, HIGH};
+
     public static void main(String[] args) {
-        OptionParser optionParser = new OptionParser();
-        optionParser.accepts("genelist").withRequiredArg().ofType(File.class);
+        OptionParser optionParser = new OptionParser() {{
+        accepts("genelist").withRequiredArg().required().ofType(File.class).describedAs("diffexp output");
+        accepts("expectedChange").withRequiredArg().ofType(expected_change.class).defaultsTo(expected_change.AVERAGE).describedAs("expected change in diffexp data");
+        accepts("FC").withRequiredArg().ofType(double.class).defaultsTo(1.0).describedAs("log2foldchange cutoff");
+        accepts("pval").withRequiredArg().ofType(double.class).defaultsTo(0.01).describedAs("adjusted p-value cutoff");
+        acceptsAll( asList( "h", "?" ), "show help" ).forHelp();
+        }};
 
-        OptionSet options = optionParser.parse("-genelist");
-        System.out.println(options.has("genelist"));
+        OptionSet params = optionParser.parse(args);
+        assertTrue(params.has("genelist"));
 
-        optionParser.accepts("expectedChange").withOptionalArg(); //of type enum {high, average, low}
-        optionParser.accepts("FC").withOptionalArg().ofType(Double.class);
-        optionParser.accepts("pval").withOptionalArg().ofType(Double.class);
+        File genelist = (File) params.valueOf("genelist");
 
-        System.out.println(options.valueOf("genelist"));
-        File genelist = (File) options.valueOf("genelist");
-        //print help for calling cmd
+        try {
+            optionParser.printHelpOn(System.out);
+        } catch (IOException e) {
+            new RuntimeException("Could not display help page.", e);
+        }
     }
 
 }
