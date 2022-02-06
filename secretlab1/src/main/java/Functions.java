c33cd16e7b03e396bb616ef63ee2b534695afadf
Elena Weiss <weisse@cip.ifi.lmu.de>
2022-02-06 14:53:48 +0100

    init of function for filtering significants

--- secretlab1/src/main/java/Functions.java
+++ secretlab1/src/main/java/Functions.java
@@ -1,2 +1,20 @@
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
 public class Functions {
+
+    public double FDR_cutoff = 0.05;
+    public double FC_cutoff = 1.0;
+
+    /*
+    @param hashmap og gene <-> FC, FDR
+    @return same hashmap without entries of unclear genes
+     */
+    public void filter_unclear(HashSet<Gene> gene2FCandFDR) {
+        gene2FCandFDR.stream().
+                filter(gene -> gene.fdr <= FDR_cutoff && (gene.fc >= FC_cutoff || gene.fc <= -FC_cutoff)).
+                collect(Collectors.toSet());
+        // TODO hier fehlt noch das von Gergely zum abschätzen der FDR von FC element von (a,b)
+    }
+
 }
--- secretlab1/src/main/java/Gene.java
+++ secretlab1/src/main/java/Gene.java
@@ -6,9 +6,10 @@ public class Gene {
     double fc;
     boolean is_significant;
 
-    public Gene(String gene_id, double fc, boolean is_significant) {
+    public Gene(String gene_id, double fc, double fdr, boolean is_significant) {
         this.gene_id = gene_id;
         this.fc = fc;
+        this.fdr = fdr;
         this.is_significant = is_significant;
     }
 }
