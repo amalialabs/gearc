9b12c2e3603e0e484b6af5f9efdd29c813c0198b
Elena Weiss <weisse@cip.ifi.lmu.de>
2022-04-10 16:04:26 +0200

    fixing plots

--- secretlab1/src/main/rscripts/gos_fdr_mean_quantile_SCATTER.R
+++ secretlab1/src/main/rscripts/gos_fdr_mean_quantile_SCATTER.R
@@ -18,8 +18,9 @@ outdir <- args[3]
 num_cols <- ncol(gos)
 c <- paste0("V", (round(((num_cols-2)*quantile))+2))
 print(c)
-gos$mean <- lapply(go[,3:num_cols], mean)
+gos$mean <- apply(gos[,3:num_cols], 1, mean)
 
-ggplot(gos, aes(x=-log10(mean), y=-log10(c))) + geom_jitter() +
-ylab(paste0("-log10(", quantile , "FDR)")) + xlab("-log10(mean FDR)") + geom_abline(col="red", lty=2)
+ggplot(gos, aes_string(x="mean", y=c)) + geom_jitter() +
+ylab(paste0("-log10(", quantile , "FDR)")) + xlab("-log10(mean FDR)") + geom_abline(col="red", lty=2) +
+scale_x_continuous(trans="log10") + scale_y_continuous(trans="log10")
 ggsave(paste0(outdir, .Platform$file.sep, "gos_mean_vs_quantile_fdr_scatter.pdf"), width=10, height=10)
