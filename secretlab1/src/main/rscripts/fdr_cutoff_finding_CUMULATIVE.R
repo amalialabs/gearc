db053aa420f2eccc8f255012cd92b428249bfb48
Elena Weiss <weisse@cip.ifi.lmu.de>
2022-05-01 16:01:56 +0200

    test

--- secretlab1/src/main/rscripts/fdr_cutoff_finding_CUMULATIVE.R
+++ secretlab1/src/main/rscripts/fdr_cutoff_finding_CUMULATIVE.R
@@ -12,6 +12,8 @@ if (length(args)==0) {
 genes <- read.csv(args[1], sep="\t", header=TRUE)
 outdir <- args[2]
 
+genes <- subset(genes, genes$is_unclear == "false")
+
 genes <- genes[,c(1,2,6)]
 
 fdrcutoff <- 0.05 #* nrow(genes)
@@ -22,33 +24,36 @@ num_five_percent_nonsig <- round(0.05 * num_nonsig_genes)
 
 num_extend <- min(num_one_percent_sig, num_five_percent_nonsig)
 
-num_last_gene <- num_sig_genes + num_extend
+num_last_gene <- min(num_sig_genes + num_extend, nrow(genes))
 num_diff_to_center <- num_last_gene - num_sig_genes
-num_first_gene <- num_sig_genes - num_extend
+num_first_gene <- max(num_sig_genes - num_extend, 0)
 
 
 ggplot(genes, aes(FDR)) + stat_ecdf(geom="step") + ylab("% genes") + xlab("FDR") +
     geom_hline(yintercept=fdrcutoff, col="red") + geom_point(aes(num_first_gene, genes[num_first_gene, 2], col="blue")) +
-    geom_point(aes(num_last_gene, genes[num_last_gene, 2], col="blue"))
+    geom_point(aes(num_last_gene, genes[num_last_gene, 2], col="blue")) +
+	scale_color_identity()
 ggsave(paste0(outdir, .Platform$file.sep, "fdr_cutoff_finding_CUMULATIVE.pdf"), width=10, height=10)
 
 
-num_last_gene <- num_sig_genes + num_one_percent_sig
-num_first_gene <- num_sig_genes - num_one_percent_sig
+num_last_gene <- min(num_sig_genes + num_one_percent_sig, nrow(genes))
+num_first_gene <- max(num_sig_genes - num_one_percent_sig, 0)
 
 ggplot(genes, aes(FDR)) + stat_ecdf(geom="step") + ylab("% genes") + xlab("FDR") +
     geom_hline(yintercept=fdrcutoff, col="red") + geom_point(aes(num_first_gene, genes[num_first_gene, 2], col="blue")) +
-    geom_point(aes(num_last_gene, genes[num_last_gene, 2], col="blue"))
+    geom_point(aes(num_last_gene, genes[num_last_gene, 2], col="blue")) +
+	scale_color_identity()
 ggsave(paste0(outdir, .Platform$file.sep, "fdr_cutoff_finding_one_percent_siggenes_ontop_CUMULATIVE.pdf"), width=10, height=10)
 
 
 
 
-num_last_gene <- num_sig_genes + num_five_percent_nonsig
-num_first_gene <- num_sig_genes - num_five_percent_nonsig
+num_last_gene <- min(num_sig_genes + num_five_percent_nonsig, nrow(genes))
+num_first_gene <- max(num_sig_genes - num_five_percent_nonsig, 0)
 
 ggplot(genes, aes(FDR)) + stat_ecdf(geom="step") + ylab("% genes") + xlab("FDR") +
     geom_hline(yintercept=fdrcutoff, col="red") + geom_point(aes(num_first_gene, genes[num_first_gene, 2], col="blue")) +
-    geom_point(aes(num_last_gene, genes[num_last_gene, 2], col="blue"))
+    geom_point(aes(num_last_gene, genes[num_last_gene, 2], col="blue")) +
+	scale_color_identity()
 ggsave(paste0(outdir, .Platform$file.sep, "fdr_cutoff_finding_five_percent_nonsiggenes_ontop_CUMULATIVE.pdf"), width=10, height=10)
 
